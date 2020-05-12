7d9bd491326142f8841bf24fab2db7dc2bdd4a81
MinexAutomation <minex.automation@gmail.com>
2020-05-12 16:55:39 -0700

    Added constructors, changed to use a version-string (not Version, since package versions might contain ranges and suffixes).

--- source/R5T.Gloucester.Types/Code/Classes/PackageReference.cs
+++ source/R5T.Gloucester.Types/Code/Classes/PackageReference.cs
@@ -6,6 +6,18 @@ namespace R5T.Gloucester.Types
     public class PackageReference
     {
         public string Name { get; set; }
-        public Version Version { get; set; }
+        // NOT a Version since packages can have version ranges, and/or suffixes ("-rc", "-beta1").
+        public string VersionString { get; set; }
+
+
+        public PackageReference()
+        {
+        }
+
+        public PackageReference(string name, string versionString)
+        {
+            this.Name = name;
+            this.VersionString = versionString;
+        }
     }
 }
