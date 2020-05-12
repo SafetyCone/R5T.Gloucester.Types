d7d8bb86382edb415ddb2441e043e18e6b6989e4
MinexAutomation <minex.automation@gmail.com>
2020-05-12 16:54:43 -0700

    Added constructors.

--- source/R5T.Gloucester.Types/Code/Classes/ProjectReference.cs
+++ source/R5T.Gloucester.Types/Code/Classes/ProjectReference.cs
@@ -6,5 +6,15 @@ namespace R5T.Gloucester.Types
     public class ProjectReference
     {
         public string ProjectFilePath { get; set; }
+
+
+        public ProjectReference()
+        {
+        }
+
+        public ProjectReference(string projectFilePath)
+        {
+            this.ProjectFilePath = projectFilePath;
+        }
     }
 }
