f600d19fd72bf9ab3b9f52f165128c87fd8fa1e1
MinexAutomation <minex.automation@gmail.com>
2020-06-10 22:26:52 -0700

    Integrated IVisualStudioProjectFile and related types (R5T.T0003).

--- source/R5T.Gloucester.Types/Code/Classes/ProjectFile.cs
+++ source/R5T.Gloucester.Types/Code/Classes/ProjectFile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using R5T.T0002;
 using R5T.T0003;
 
 using R5T.Ostersund;
@@ -8,7 +9,7 @@ using R5T.Ostersund;
 
 namespace R5T.Gloucester.Types
 {
-    public class ProjectFile
+    public class ProjectFile : IVisualStudioProjectFile
     {
         // Provide Settable<T> instances to make sure we don't have a null references.
         public Settable<string> SDK { get; set; } = new Settable<string>();
@@ -20,7 +21,7 @@ namespace R5T.Gloucester.Types
         public Settable<OutputType> OutputType { get; set; } = new Settable<OutputType>();
         public Settable<TargetFramework> TargetFramework { get; set; } = new Settable<TargetFramework>();
 
-        public List<ProjectReference> ProjectReferences { get; } = new List<ProjectReference>();
-        public List<PackageReference> PackageReferences { get; } = new List<PackageReference>();
+        public IList<ProjectReference> ProjectReferences { get; } = new List<ProjectReference>();
+        public IList<PackageReference> PackageReferences { get; } = new List<PackageReference>();
     }
 }
